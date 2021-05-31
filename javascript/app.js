ab7f946f7f088a35ff0257890f8c0cc08d16d56a
ayazahra <>
2021-05-31 06:39:06 +0000

    lab5

---
+++ javascript/app.js
@@ -0,0 +1,39 @@
+var userName ;
+var age = "21";
+
+var userName = prompt("Whats your name ?");
+alert("welcome to our websit " + userName );
+
+
+var ageinput = prompt("What is your fav animals ?");
+var favAnimals="cat";//console.log(userAge);
+var userAge= prompt("what your old ?");
+alert("welcom to our websit"+ userAge);
+
+if(favAnimals==
+ "cat" ){alert('yes it is not this');}else{alert('no it is not this')}
+document.write('<img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fd9%2Fcd%2F69%2Fd9cd69b8fe5c67c052af138b04a6589d.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F832603049838012776%2F&tbnid=HWDoxfn2rqCj8M&vet=12ahUKEwiTuv-Ym-_wAhUXtaQKHZD2CC0QMygregUIARCTAg..i&docid=ZwPHdpkpBfA9rM&w=620&h=326&q=%D8%B5%D9%88%D8%B1%20%D9%82%D8%B7%D8%A9&safe=active&ved=2ahUKEwiTuv-Ym-_wAhUXtaQKHZD2CC0QMygregUIARCTAg"/>');
+if(ageinput == age ){
+  // console.log("what do you work ?");
+  alert('excatily');
+}else { alert('my old 21 years');
+}
+
+var catsname = ['liza','fella','karak','milo',15 ,true, ['code101', 'code102', 'code201']];
+// console.log(catsname);
+// console.log(catsname[1]);
+// console.log(catsname[3]);
+
+// console.log(catsname[0], ',' ,studentsName[2])
+
+// console.log(catsname.length)
+// Start point
+// Comparing between the start point and end point (Count oof the value inside the array)
+// i = i + 1 (increase the i var)
+
+
+
+for(var i = 0; i < catsname.length; i++) { document.write('<img src = "https://images.theconversation.com/files/323930/original/file-20200330-146712-1fsv5cs.jpg?ixlib=rb-1.1.0&rect=8%2C33%2C5609%2C3705&q=45&auto=format&w=496&fit=clip"/>') } 
+var ageinput = prompt("What is your cat's name ?");
+
+while (userAge != 21 ) {userAge = prompt ('please enter the age 21 !!!!');}
